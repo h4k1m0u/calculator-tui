b7e19e4c8931e6cc26b89c4817325cec53e0176d
h4k1m0u <h.benoudjit@gmail.com>
2021-06-04 00:16:17 +0200

    Menu class & item navigation with arrow keys

--- README.md
+++ README.md
@@ -2,9 +2,11 @@
 - [Official python API][official-api]
 - [Official tutorial][official-tutorial]
 - [Tutorial][tuturial]
-- [Exmaples][examples]
+- [Examples][examples]
+- [Difference between newwin and subwin][newwin-vs-subwin]
 
 [official-api]: https://docs.python.org/3/library/curses.html
 [official-tutorial]: https://docs.python.org/3/howto/curses.html
 [tutorial]: https://www.devdungeon.com/content/curses-programming-python
 [examples]: https://hg.python.org/cpython/file/2.7/Demo/curses/
+[newwin-subwin]: http://www.linuxfocus.org/English/March2002/article233.shtml
--- script.py
+++ script.py
@@ -16,12 +16,45 @@ def make_panel(content, nlines, ncols, y, x):
     return panel
 
 
+class Menu:
+    def __init__(self, stdscr: 'curses._CursesWindow'):
+        """
+        @param stdscr parent window
+        """
+        self.items = ['Run game', 'Options', 'Quit game']
+        self.window = stdscr.subwin(0, 0)
+        self.position = 0
+
+        # needed so curses can interpret arrow keys presses
+        self.window.keypad(1)
+
+    def draw(self):
+        while True:
+            # render menu items (active/inactive)
+            for i_item, item in enumerate(self.items):
+                if i_item == self.position:
+                    mode = curses.A_REVERSE
+                else:
+                    mode = curses.A_NORMAL
+
+                self.window.addstr(i_item, 0, item, mode)
+
+            # menu navigation with arrow keys presses
+            key = self.window.getch()
+            if key == curses.KEY_UP and self.position != 0:
+                self.position = self.position - 1
+            elif key == curses.KEY_DOWN and self.position != len(self.items) - 1:
+                self.position = self.position + 1
+            elif key == ord('q'):
+                break
+
+
 def main(stdscr: 'curses._CursesWindow'):
     """
     Called by curses.wrapper() & restores terminal state at end of execution
     Explicit type hint for `stdscr` for autocomplete
 
-    @param stdscr  Main screen (Window) to print onto
+    @param stdscr Main screen (Window) to print onto
     """
     # initialize & define terminal colors
     curses.start_color()
@@ -32,6 +65,10 @@ def main(stdscr: 'curses._CursesWindow'):
     curses.curs_set(0)
 
     # main loop
+    menu = Menu(stdscr)
+    menu.draw()
+
+    """
     key = ''
     while key != ord('q'):
         # TODO: print a menu using panels
@@ -39,14 +76,6 @@ def main(stdscr: 'curses._CursesWindow'):
         panel1 = make_panel('Panel1', 10, 10, 0, 0)
         panel2 = make_panel('Panel2', 10, 10, 5, 5)
         curses.panel.update_panels()
-        stdscr.refresh()
-        
-        time.sleep(1)
-
-        panel1.top()
-        curses.panel.update_panels()
-
-        # stdscr.box()
 
         # terminal dimensions
         n_rows, n_cols = stdscr.getmaxyx()
@@ -67,6 +96,7 @@ def main(stdscr: 'curses._CursesWindow'):
 
         # wait for keypress
         key = stdscr.getch()
+    """
 
 
 if __name__ == '__main__':
